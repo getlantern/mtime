4e487ed63040ff91f58e7357990b9fd9efba7887
Ox Cart <ox.to.a.cart@gmail.com>
2017-01-05 09:34:42 -0600

    Added Instant abstraction

--- stopwatch.go
+++ stopwatch.go
@@ -8,21 +8,36 @@ import (
 	"time"
 )
 
-// Start starts a stopwatch and returns a function that itself returns the
-// amount of time elapsed since the start of the stopwatch.
-func Start() (elapsed func() time.Duration) {
-	start := Now()
-	return func() time.Duration {
-		return Elapsed(start)
-	}
+// An Instant represents an instant in monotonically increasing time
+type Instant uint64
+
+// Add adds a duration to an Instant
+func (i Instant) Add(d time.Duration) Instant {
+	return Instant(uint64(time.Duration(i) + d))
+}
+
+// Sub subtracts an Instant from an Instant
+func (i Instant) Sub(o Instant) time.Duration {
+	return time.Duration(i - o)
+}
+
+// ToTime converts an Instant to the equivalent time.Time
+func (i Instant) ToTime() time.Time {
+	s := int64(i) / int64(time.Second)
+	ns := int64(i) % int64(time.Second)
+	return time.Unix(s, ns)
 }
 
 // Now() returns an instant in monotonic time
-func Now() uint64 {
-	return monotime.Now()
+func Now() Instant {
+	return Instant(monotime.Now())
 }
 
-// Elapsed calculates the amount of time that has elapsed since an Instant
-func Elapsed(start uint64) time.Duration {
-	return time.Duration(monotime.Now() - start)
+// Stopwatch starts a stopwatch and returns a function that itself returns the
+// amount of time elapsed since the start of the stopwatch.
+func Stopwatch() (elapsed func() time.Duration) {
+	start := Now()
+	return func() time.Duration {
+		return Now().Sub(start)
+	}
 }
--- stopwatch_test.go
+++ stopwatch_test.go
@@ -6,8 +6,19 @@ import (
 	"time"
 )
 
+func TestInstant(t *testing.T) {
+	delta := 1 * time.Millisecond
+	i1 := Now()
+	t1 := time.Now()
+	i2 := i1.Add(delta)
+	assert.True(t, i2 > i1)
+	assert.Equal(t, i1, i2.Add(-1*delta))
+	assert.Equal(t, delta, i2.Sub(i1))
+	assert.True(t, i1.ToTime().Sub(t1) < 100*time.Millisecond, "Monotonic instant and actual time should be close")
+}
+
 func TestStopwatch(t *testing.T) {
-	elapsed := Start()
+	elapsed := Stopwatch()
 	time.Sleep(100 * time.Millisecond)
 	e1 := elapsed()
 	time.Sleep(100 * time.Millisecond)
